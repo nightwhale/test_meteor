697c008b2b95f5cbe642d3f6a02595a93661ecc2
조현 <hyun@cordial.com>
2020-07-01 13:03:26 +0900

    shell add

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "set mongodb"
 #export MONGO_URL="mongodb://worksUsername:worksPassword@192.168.100.192:14017/worksDatabase?authSource=admin"
-export MONGO_URL="mongodb://works:zheldjf@192.168.10.135:27017/works?authSource=works"
+#export MONGO_URL="mongodb://works:zheldjf@192.168.10.135:27017/works?authSource=works"
+#export MONGO_URL="mongodb://worksUsername:worksPassword@asolution.iptime.org:27017/?authSource=works&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+export MONGO_URL="mongodb://worksUsername:worksPassword@asolution.iptime.org:27017/?authSource=works"
 echo "start meteor"
 meteor
 
---
+++ stop.sh
@@ -0,0 +1,2 @@
+echo "stop process"
+kill -9 `ps ax | grep node | grep meteor | awk '{print $1}'`
